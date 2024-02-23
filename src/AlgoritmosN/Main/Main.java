5236b97fbd77c4134f494408fdd094ebe2f7e8cb
alberba <albertsalomv@gmail.com>
2024-02-23 12:41:42 +0100

    Implementación clase Main

---
+++ src/AlgoritmosN/Main/Main.java
@@ -0,0 +1,62 @@
+package AlgoritmosN.Main;
+
+import AlgoritmosN.NotiEnum;
+import AlgoritmosN.Notificacion;
+import AlgoritmosN.Vista.Vista;
+import AlgoritmosN.Modelo.Modelo;
+import AlgoritmosN.Controlador.Controlador;
+import mesurament.Mesurament24;
+
+
+public class Main implements Notificacion {
+    private final Vista vista;
+    private final Modelo modelo;
+    private Controlador controlador;
+
+    public Main() {
+        controlador = null;
+        modelo = new Modelo(this);
+        vista = new Vista("AlgoritmosN", this);
+        vista.mostrar();
+    }
+
+    public static void main(String[] args) {
+        //Mesurament24.mesura();
+        new Main();
+    }
+
+    @Override
+    public void notificar(NotiEnum s) {
+        switch (s) {
+            case INICIAR:
+                if (controlador == null) {
+                    controlador = new Controlador(this);
+                    controlador.start();
+                }
+                break;
+            case PARAR:
+                controlador = null;
+                vista.repaint();
+                break;
+            case DIBUJAR:
+                vista.repaint();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public Vista getVista() {
+        return vista;
+    }
+
+    public Modelo getModelo() {
+        return modelo;
+    }
+
+    public Controlador getControlador() {
+        return controlador;
+    }
+
+
+}
