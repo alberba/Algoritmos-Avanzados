8abb7ea51bb572832b4781b3c4301986a41373d5
AdOnDe-BoY <106172989+AdOnDe-BoY@users.noreply.github.com>
2024-04-03 18:55:52 +0200

    Changes by alber

--- Practica3/src/Controlador/Controlador.java
+++ Practica3/src/Controlador/Controlador.java
@@ -1,5 +1,6 @@
 package Controlador;
 
+import Controlador.TreeSort.TreeSort;
 import Modelo.Distribucion;
 import Modelo.Modelo;
 import Main.Main;
@@ -7,7 +8,9 @@ import Notification.NotiEnum;
 import Notification.Notificacion;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.Collections;
 
 public class Controlador extends Thread implements Notificacion {
 
@@ -15,11 +18,13 @@ public class Controlador extends Thread implements Notificacion {
     private boolean interrumpir = false;
     private double pasos = 0;
     private static int nPasosTotal = 0;
-    private ArrayList<Integer> datos;
+    private ArrayList<Double> datos;
+    private long [] tiempos;
 
     public Controlador(Main p) {
         prog = p;
         prog.getModelo().reset();
+        tiempos = new long[4];
     }
 
     public void run() {
@@ -28,45 +33,45 @@ public class Controlador extends Thread implements Notificacion {
         int n = modelo.getN();
         datos = new ArrayList<>();
         Random rand = new Random();
-
-        switch (dist) { // Se generan los datos en función de la distribución
+        // Se generan los datos en función de la distribución
+        switch (dist) {
             case UNIFORME:
                 for (int i = 0; i < n; i++) {
-                    datos.add((int) (Math.random() * 100));
+                    datos.add(Math.random());
                 }
                 break;
             case GAUSSIANA:
+                double num;
                 for (int i = 0; i < n; i++) {
-                    datos.add((int) (rand.nextGaussian() * 100));
+                    num = rand.nextGaussian();
+                    // Si el valor se aleja mucho de la media, se descarta el valor
+                    while (num < -3 || num > 3) {
+                        num = rand.nextGaussian();
+                    }
+                    // Se normalizan los datos, serán valores entre 0 y 1
+                    datos.add((rand.nextGaussian() + 3) / 6);
                 }
                 break;
             case EXPONENCIAL:
 
                 break;
         }
-
         // Se ordenan los datos con cada uno de los algoritmos, se calcula el tiempo y se añade al modelo
         // TIN //
-        // Se obtiene el tiempo actual
-        long tin = System.nanoTime();
-        // Se ordenan los datos
+        long t = System.nanoTime();
+        timsort(new ArrayList<>(datos));
+        tiempos[0] = System.nanoTime() - t;
 
-        // Se obtiene el tiempo actual
-        long tiempo = System.nanoTime() - tin;
-        switch (dist) {
-            case UNIFORME: // Buckets uniformes
+        // BUCKET //
+        t = System.nanoTime();
+        bucketSort(new ArrayList<>(datos), dist);
+        tiempos[1] = System.nanoTime() - t;
 
-                break;
-            case GAUSSIANA: // Buckets adecuados a la gaussiana
-
-                break;
-            case EXPONENCIAL:
-                break;
-        }
-
-        // METODOS DE ORDENACIÓN
-        long tini = System.nanoTime();
-
+        // TREESORT //
+        t = System.nanoTime();
+        TreeSort treeSort = new TreeSort();
+        treeSort.sort(new ArrayList<>(datos));
+        tiempos[2] = System.nanoTime() - t;
 
         if (!interrumpir) {
             prog.notificar(NotiEnum.PARAR, null);
@@ -74,6 +79,57 @@ public class Controlador extends Thread implements Notificacion {
         } else {
             // Se notifica mediante la consola de salida que se ha interrumpido el proceso
             System.out.println("Interrumpido");
+        }
+    }
+
+    private void timsort(ArrayList<Double> arr) {
+        Collections.sort(arr);
+    }
+
+    private void bucketSort(ArrayList<Double> arr, Enum<Distribucion> dist) {
+        int nDatos = arr.size();
+        // Se generan los buckets
+        List<List<Double>> buckets = new ArrayList<>();
+        int numBuckets = (int) Math.sqrt(nDatos); // Número de buckets más indicado = sqrt(n)
+        for (int i = 0; i < numBuckets; i++) {
+            buckets.add(new ArrayList<>());
+        }
+        // Según el tipo de distribución, se añaden los elementos a los buckets de una forma u otra
+        if (dist == Distribucion.UNIFORME) {
+            for (double element : arr) {
+                int indexBucket = (int) element * nDatos;
+                buckets.get(indexBucket).add(element);
+            }
+        } else {
+            for (double element : arr) {
+                if (element < 0.3) {
+                    // 9% de los buckets
+                } else if (element < 0.4) {
+                    // 16% de los buckets
+                } else if (element < 0.5) {
+                    // 25% de los buckets
+                } else if (element < 0.6) {
+                    // 25% de los buckets
+                } else if (element < 0.7) {
+                    // 16% de los buckets
+                } else {
+                    // 9% de los buckets
+                }
+                int indexBucket = (int) Math.pow(element, 2) * numBuckets;
+                buckets.get(indexBucket).add(element);
+            }
+        }
+
+
+        for (int i = 0; i < nDatos; i++) {
+            Collections.sort(buckets.get(i));
+        }
+
+        int index = 0;
+        for (int i = 0; i < nDatos; i++) {
+            for (int j = 0; j < nDatos; j++) {
+                arr.set(index++, buckets.get(i).get(j));
+            }
         }
     }
 
